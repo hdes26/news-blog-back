015ec123d94a4b9d03d73ec480fa5b4006ad07ee
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-28 09:18:20 -0500

    [ADD] entities migration :zap:

--- src/database/migrations/entities-migration.ts
+++ src/database/migrations/entities-migration.ts
@@ -1,9 +1,12 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migration1690553656773 implements MigrationInterface {
-  name = 'Migration1690553656773';
+export class Migration1690553841431 implements MigrationInterface {
+  name = 'Migration1690553841431';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "author" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "email" character varying(50) NOT NULL, CONSTRAINT "PK_5a0e79799d372fe56f2f3fa6871" PRIMARY KEY ("id"))`,
+    );
     await queryRunner.query(
       `CREATE TABLE "label" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "description" character varying(255) NOT NULL, CONSTRAINT "PK_5692ac5348861d3776eb5843672" PRIMARY KEY ("id"))`,
     );
@@ -12,6 +15,9 @@ export class Migration1690553656773 implements MigrationInterface {
     );
     await queryRunner.query(
       `CREATE TABLE "news" ("id" SERIAL NOT NULL, "title" character varying(255) NOT NULL, "content" character varying(255) NOT NULL, "news_date" TIMESTAMP NOT NULL DEFAULT now(), "author" integer, "label" integer, "category" integer, CONSTRAINT "PK_39a43dfcb6007180f04aff2357e" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "comment" ("id" SERIAL NOT NULL, "text" character varying(255) NOT NULL, "comment_date" TIMESTAMP NOT NULL DEFAULT now(), "news" integer, "author" integer, CONSTRAINT "PK_0b0e4bbc8415ec426f87f3a88e2" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "news" ADD CONSTRAINT "FK_2b25d3952694bca90d83e2342bd" FOREIGN KEY ("author") REFERENCES "author"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
@@ -25,9 +31,15 @@ export class Migration1690553656773 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "comment" ADD CONSTRAINT "FK_2891d412a4c04124f07973d39ce" FOREIGN KEY ("news") REFERENCES "news"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
+    await queryRunner.query(
+      `ALTER TABLE "comment" ADD CONSTRAINT "FK_d5b5ce13b7dc246707937a9ced2" FOREIGN KEY ("author") REFERENCES "author"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "comment" DROP CONSTRAINT "FK_d5b5ce13b7dc246707937a9ced2"`,
+    );
     await queryRunner.query(
       `ALTER TABLE "comment" DROP CONSTRAINT "FK_2891d412a4c04124f07973d39ce"`,
     );
@@ -40,8 +52,10 @@ export class Migration1690553656773 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "news" DROP CONSTRAINT "FK_2b25d3952694bca90d83e2342bd"`,
     );
+    await queryRunner.query(`DROP TABLE "comment"`);
     await queryRunner.query(`DROP TABLE "news"`);
     await queryRunner.query(`DROP TABLE "category"`);
     await queryRunner.query(`DROP TABLE "label"`);
+    await queryRunner.query(`DROP TABLE "author"`);
   }
 }
